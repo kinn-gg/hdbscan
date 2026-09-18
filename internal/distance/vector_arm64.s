72f43f21689b8b7fb10bcc622529ab19fb567794
Keenan Nemetz <keenan.nemetz@gmail.com>
2026-09-18 09:14:21 -0700

    fix: support ARM64 kernels on Go 1.24

--- internal/distance/vector_arm64.s
+++ internal/distance/vector_arm64.s
@@ -12,12 +12,12 @@ TEXT ·dotVector(SB), NOSPLIT, $16-32
 dot_loop:
 	VLD1.P 16(R0), [V0.D2]
 	VLD1.P 16(R1), [V1.D2]
-	VFMUL V1.D2, V0.D2, V0.D2
-	VFADD V0.D2, V2.D2, V2.D2
+	WORD $0x6e61dc00 // FMUL V0.2D, V0.2D, V1.2D
+	WORD $0x4e60d442 // FADD V2.2D, V2.2D, V0.2D
 	SUB $1, R3
 	CBNZ R3, dot_loop
 dot_tail:
-	VFADDP V2.D2, V2.D2, V2.D2
+	WORD $0x6e62d442 // FADDP V2.2D, V2.2D, V2.2D
 	VST1 [V2.D2], (RSP)
 	FMOVD 0(RSP), F2
 	TBZ $0, R2, dot_done
@@ -39,13 +39,13 @@ TEXT ·squaredEuclideanVector(SB), NOSPLIT, $16-32
 dist_loop:
 	VLD1.P 16(R0), [V0.D2]
 	VLD1.P 16(R1), [V1.D2]
-	VFSUB V1.D2, V0.D2, V0.D2
-	VFMUL V0.D2, V0.D2, V0.D2
-	VFADD V0.D2, V2.D2, V2.D2
+	WORD $0x4ee1d400 // FSUB V0.2D, V0.2D, V1.2D
+	WORD $0x6e60dc00 // FMUL V0.2D, V0.2D, V0.2D
+	WORD $0x4e60d442 // FADD V2.2D, V2.2D, V0.2D
 	SUB $1, R3
 	CBNZ R3, dist_loop
 dist_tail:
-	VFADDP V2.D2, V2.D2, V2.D2
+	WORD $0x6e62d442 // FADDP V2.2D, V2.2D, V2.2D
 	VST1 [V2.D2], (RSP)
 	FMOVD 0(RSP), F2
 	TBZ $0, R2, dist_done
